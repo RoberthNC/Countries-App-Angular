92fbe70ef88546feab1da7c8c6157b66e3e19857
Roberto Núñez <Roberto>
2024-09-24 11:26:01 -0500

    Design and dynamic function works partially

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+})
+export class SearchBoxComponent {
+  @Input('placeholder')
+  public placeholder: string = '';
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+}
